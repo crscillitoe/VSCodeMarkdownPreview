1ff9e0ea6869f55b9bb18b47bcd385cc2bf0ea35
Bradford Bonanno <bwbonanno@wpi.edu>
2019-09-07 14:47:07 -0400

    Split on open of md file

--- markdownpreviewopener/src/extension.ts
+++ markdownpreviewopener/src/extension.ts
@@ -1,26 +1,16 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function isMarkdownFile(doc: vscode.TextDocument) {
+	const split = doc.fileName.split('.');
+	const fileExtension = split[split.length - 1];
+	return fileExtension === "md" || doc.languageId === "markdown";
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdownpreviewopener" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	vscode.workspace.onDidOpenTextDocument((doc: vscode.TextDocument) => {
+		if (!isMarkdownFile(doc)) { return; }
+		vscode.commands.executeCommand("markdown.showPreviewToSide");
 	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
