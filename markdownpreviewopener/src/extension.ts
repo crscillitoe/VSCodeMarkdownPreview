5c97c1d828c5ed89b71c0a7cab6ffc6078922f7a
Bradford Bonanno <bwbonanno@wpi.edu>
2019-09-08 15:27:34 -0400

    Code cleanup

--- markdownpreviewopener/src/extension.ts
+++ markdownpreviewopener/src/extension.ts
@@ -1,17 +1,22 @@
 import * as vscode from 'vscode';
 
 function isMarkdownFile(doc: vscode.TextDocument) {
-	const split = doc.fileName.split('.');
+	const split = doc.fileName.split('.')
+		.filter((substr) => substr !== "git");
 	const fileExtension = split[split.length - 1];
 	return fileExtension === "md" || doc.languageId === "markdown";
 }
 
-export function activate(context: vscode.ExtensionContext) {
-	vscode.workspace.onDidOpenTextDocument((doc: vscode.TextDocument) => {
-		if (!isMarkdownFile(doc)) { return; }
-		vscode.commands.executeCommand("markdown.showPreviewToSide");
-	});
+function openPreviewIfMarkdown(doc: vscode.TextDocument) {
+	if (!isMarkdownFile(doc)) { return; }
+	vscode.commands.executeCommand("markdown.showPreviewToSide");
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.workspace.onDidOpenTextDocument((doc: vscode.TextDocument) => {
+		openPreviewIfMarkdown(doc);
+	});
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
